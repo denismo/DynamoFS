927e16b522e86e63ae3b7f7a80363ce6aa2f98b8
Denis Mikhalkin <denismo@yahoo.com>
2013-04-25 23:11:42 +1000

    Implemented: chmod, chown, symlink, readlink Partial: rename

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Linux FUSE file system implementation with AWS DynamoDB as the storage
 
 Overview
 ========
-**Note: This project is in its early R&D stage. Various design and implementations are being tried for file system operations
+**Note: This project is in its early R&D stage. Various designs and implementation strategies are being tried for file system operations
 so any practical usage is not recommended.**
--- dynamofs.py
+++ dynamofs.py
@@ -4,7 +4,7 @@ from __future__ import with_statement
 
 __author__ = 'Denis Mikhalkin'
 
-from errno import EACCES,ENOENT
+from errno import EACCES,ENOENT,EINVAL,EEXIST
 from os.path import realpath
 from sys import argv, exit
 from threading import Lock
@@ -27,11 +27,50 @@ class DynamoFS(LoggingMixIn, Operations):
         self.__createRoot()
 
     def __createRoot(self):
+        if not self.table.has_item("/", "/"):
+            self.mkdir("/", 0755)
+
+    def chmod(self, path, mode):
         try:
-            self.rmdir("/")
-        except:
-            pass
-        self.mkdir("/", 0x755)
+            self.table.get_item(os.path.dirname(path), name, attributes_to_get=['name'])
+        except DynamoDBKeyNotFoundError:
+            raise FuseOSError(ENOENT)
+        else:
+            item['st_mode'] &= 0770000
+            item['st_mode'] |= mode
+            item.update()
+            return 0
+
+    def chown(self, path, uid, gid):
+        try:
+            self.table.get_item(os.path.dirname(path), name, attributes_to_get=['name'])
+        except DynamoDBKeyNotFoundError:
+            raise FuseOSError(ENOENT)
+        else:
+            item['st_uid'] = uid
+            item['st_gid'] = gid
+            item.update()
+
+    def open(self, path, flags):
+        # TODO read/write locking? Permission check?
+        try:
+            self.table.get_item(os.path.dirname(path), name, attributes_to_get=['name'])
+        except DynamoDBKeyNotFoundError:
+            raise FuseOSError(ENOENT)
+        else:
+            return self.__allocId()
+
+    def utimens(self, path, times=None):
+        now = int(time())
+        atime, mtime = times if times else (now, now)
+        try:
+            item = self.table.get_item(os.path.dirname(path), name)
+        except DynamoDBKeyNotFoundError:
+            raise FuseOSError(ENOENT)
+        else:
+            item['st_atime'] = atime
+            item['st_mtime'] = mtime
+            item.update()
 
     def getattr(self, path, fh=None):
         print "getattr(%s)" % path
@@ -44,8 +83,11 @@ class DynamoFS(LoggingMixIn, Operations):
             raise FuseOSError(ENOENT)
         return item
 
-    def readdir(self, path, fh):
+    def readdir(self, path, fh=None):
         print "readdir(%s)" % path
+        # Verify the directory exists
+        self.__checkFileDirExists(path)
+
         yield '.'
         yield '..'
         items = self.table.query(path, attributes_to_get=['name'])
@@ -56,31 +98,11 @@ class DynamoFS(LoggingMixIn, Operations):
                 continue # This could be the folder itself
             yield entry['name']
 
-    def __readdir(self, path, fh):
-        print "readdir(%s)" % path
-        item = self.table.get_item(path, range_key=None, attributes_to_get=['children'])
-        if item == None:
-            print "readdir: No such entry %s" % path
-            raise FuseOSError(ENOENT)
-        yield '.'
-        yield '..'
-        if not "children" in item:
-            print "readdir: %s has no children" % path
-            return
-
-        for entry in item['children']:
-            child = self.table.get_item(entry, range_key=None, attributes_to_get=['name'])
-            if child == None:
-                # Modifed underneath
-                continue
-            else:
-                yield child.Name
-
     def mkdir(self, path, mode):
         print "mkdir(%s)" % path
         self.create(path, S_IFDIR | mode)
 
-    # TODO Check if it is empty or remove everything?
+    # TODO Check if it is empty
     def rmdir(self, path):
         print "rmdir(%s)" % path
         name=os.path.basename(path)
@@ -88,9 +110,45 @@ class DynamoFS(LoggingMixIn, Operations):
             name="/"
         self.table.get_item(os.path.dirname(path), name).delete()
 
+    def rename(self, old, new):
+        if old == new: return
+        # TODO Check permissions in directories
+        item = self.__checkFileExists(old)
+        newItem = self.__getItemOrNone(new)
+        if self.__isDirectory(newItem):
+            item.hash_key = new
+            item.update()
+        elif self.__isFile(newItem):
+            raise FuseOSError(EEXIST)
+        else:
+            item.hash_key = os.path.dirname(new)
+            item.range_key = os.path.basename(new)
+            item.update()
+
+    def readlink(self, path):
+        item = self.__checkFileExists(path)
+        if not "symlink" in item:
+            raise FuseOSError(EINVAL)
+        return item["symlink"]
+
+    def symlink(self, target, source):
+        # TODO: Verify does not exist
+        # TODO: Update parent directory time
+        name=os.path.basename(target)
+        if name == "":
+            name="/"
+        attrs={'name': name, 'path': os.path.dirname(target),
+               'st_mode': S_IFLNK | 0777, 'st_nlink': 1,
+               'symlink': source
+               }
+        item = self.table.new_item(attrs=attrs)
+        item.put()
+        return self.__allocId()
+
     def create(self, path, mode, fh=None):
         print "create(%s, %d)" % (path, mode)
-        id = self.__allocId()
+        # TODO: Verify does not exist
+        # TODO: Update parent directory time
         l_time=int(time())
         name=os.path.basename(path)
         if name == "":
@@ -98,19 +156,32 @@ class DynamoFS(LoggingMixIn, Operations):
         attrs={'name': name, 'path': os.path.dirname(path),
                'st_mode': mode, 'st_nlink': 1,
                'st_size': 0, 'st_ctime': l_time, 'st_mtime': l_time,
-               'st_atime': l_time, 'id': id}
+               'st_atime': l_time}
         if mode & S_IFDIR == 0:
             mode |= S_IFREG
             attrs["mode"] = mode
         item = self.table.new_item(attrs=attrs)
         item.put()
-        return id
+        return self.__allocId()
 
     def __allocId(self):
         idItem = self.table.new_item(attrs={'name':'counter', 'path':'global'})
         idItem.add_attribute("value", 1)
         res = idItem.save(return_values="ALL_NEW")
         return res["Attributes"]["value"]
+
+    def __checkFileDirExists(self, filepath):
+        self.__checkFileExists(os.path.dirname(filepath))
+
+    def __checkFileExists(self, filepath):
+        name=os.path.basename(filepath)
+        if name == "":
+            name="/"
+        try:
+            return self.table.get_item(os.path.dirname(filepath), name, attributes_to_get=['name'])
+        except DynamoDBKeyNotFoundError:
+            raise FuseOSError(ENOENT)
+
 
 if __name__ == '__main__':
     if len(argv) != 4:
